0bcf8d87b4ede92dd247fd7bd9c1ca27c26d9ba9
Tan Chun Siong <dreamtcs@hotmail.com>
2024-07-08 14:03:55 +0800

    updated to 6.1.0

--- demo/MeetingChatEventListener.h
+++ demo/MeetingChatEventListener.h
@@ -19,8 +19,8 @@ public:
 	
 
 	/// \brief Chat message callback. This function is used to inform the user once received the message sent by others.
-/// \param chatMsg An object pointer to the chat message.
-/// \param content A pointer to the chat message in json format. This parameter is currently invalid, hereby only for reservations. 
+		/// \param chatMsg An object pointer to the chat message.
+		/// \param content A pointer to the chat message in json format. This parameter is currently invalid, hereby only for reservations. 
 	virtual void onChatMsgNotifcation(IChatMsgInfo* chatMsg, const zchar_t* content = NULL);
 
 	/// \brief The authority of chat changes callback. This function is used to inform the user when the authority of chat changes in the meeting or webinar.
@@ -33,5 +33,17 @@ public:
 	virtual void onChatMsgDeleteNotification(const zchar_t* msgID, SDKChatMessageDeleteType deleteBy);
 
 	virtual void onShareMeetingChatStatusChanged(bool isStart);
+
+	/// \brief Invoked when start send file.
+	/// \param sender The class to send file object.
+	virtual void onFileSendStart(ISDKFileSender* sender);
+
+	/// \brief Invoked when receiving a file from another user.
+	/// \param receiver The class to receive the file object.
+	virtual void onFileReceived(ISDKFileReceiver* receiver);
+
+	/// \brief Invoked when send or receive file status change.
+	/// \param info The class to basic transfer information.
+	virtual void onFileTransferProgress(SDKFileTransferInfo* info);
 
 };
--- demo/MeetingReminderEventListener.h
+++ demo/MeetingReminderEventListener.h
@@ -2,7 +2,7 @@
 #include "zoom_sdk.h"
 
 
-
+USING_ZOOM_SDK_NAMESPACE
 
 class MeetingReminderEventListener : public ZOOM_SDK_NAMESPACE::IMeetingReminderEvent
 {
@@ -10,6 +10,15 @@ public:
     MeetingReminderEventListener();
 
   
-   virtual void onReminderNotify(ZOOMSDK::IMeetingReminderContent* content, ZOOMSDK::IMeetingReminderHandler* handle);
-    
+
+   /// \brief Callback event of the reminder dialog show.
+   /// \param content The detail content in the reminder dialog. For more details, see \link IMeetingReminderContent \endlink enum.
+   /// \param handle A pointer to the IMeetingReminderHandler. For more details, see \link IMeetingReminderHandler \endlink.
+   virtual void onReminderNotify(IMeetingReminderContent* content, IMeetingReminderHandler* handle);
+
+   /// \brief Callback event of the enable reminder dialog show.
+   /// \param content The detail content in the reminder dialog. For more details, see \link IMeetingEnableReminderHandler \endlink enum.
+   /// \param handle A pointer to the IMeetingReminderHandler. For more details, see \link IMeetingEnableReminderHandler \endlink.
+   virtual void onEnableReminderNotify(IMeetingReminderContent* content, IMeetingEnableReminderHandler* handle);
+
 };
